e78367924295016003944fd143a191e0b34b97ae
Perales33 <perales.antonio33@hotmail.com>
2024-02-04 12:36:57 +0100

    Enlaces a buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,3 +1,4 @@
+/* Javascript del Buscador: https://youtu.be/I3stKiPIb-w?si=TQ0hoW30-FeWLg5I*/
 document.addEventListener("keyup",e => {
    if(e.target.matches("#buscador")){
       document.querySelectorAll(".pagina").forEach(pagina =>{
