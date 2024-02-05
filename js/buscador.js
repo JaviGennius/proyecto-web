6356ecbc8095a32607a38b135fa135a4ade83cf0
Perales33 <perales.antonio33@hotmail.com>
2024-02-05 12:19:33 +0100

    Cambio de parametro y _black del indice

--- js/buscador.js
+++ js/buscador.js
@@ -1,6 +1,6 @@
 /* Javascript del Buscador: https://youtu.be/I3stKiPIb-w?si=TQ0hoW30-FeWLg5I*/
 document.addEventListener("keyup",parametro => {
-   if(e.target.matches("#buscador")){
+   if(parametro.target.matches("#buscador")){
       document.querySelectorAll(".pagina").forEach(pagina =>{
          pagina.textContent.toLowerCase().includes(parametro.target.value.toLowerCase())?pagina.classList.remove("filtro"):pagina.classList.add("filtro")
       })
