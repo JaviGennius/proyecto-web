26d7549905d9a17683e738432c5053dd561d23a0
JaviGennius <javierom93@gmail.com>
2024-02-11 18:27:52 +0100

    imagenesreverso yjs de flipimg.js

---
+++ js/flipimg.js
@@ -0,0 +1,18 @@
+let  fotomedico = "delante";
+function flip() {
+    let imagen = document.getElementById("medico");
+    if (fotomedico =="delante"){
+        imagen.src = "../imagenes/c2.d.png";
+        fotomedico ="detras"
+    }
+    else {
+        imagen.src ="../imagenes/cardiolog1.jpg";
+        fotomedico ="delante"
+    }
+}
+function flipout(){
+    let imagen = document.getElementById("medico");
+    
+        imagen.src ="../imagenes/cardiolog1.jpg";
+        fotomedico ="delante"
+    }
