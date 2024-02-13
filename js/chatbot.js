6d92a5a8e1a3db2be031c648926bfa3025760fcd
Perales33 <perales.antonio33@hotmail.com>
2024-02-13 18:38:23 +0100

    Cambios sin el bot (error de antonio)

--- js/chatbot.js
+++
@@ -1,9 +0,0 @@
-function openchatbot() {
-    var chatbot = document.getElementById("chat-contenedor"); // Reemplaza "parrafoId" con el ID real de tu párrafo
-
-    if (chatbot.style.display === "none") {
-        chatbot.style.display = "block";
-    } else {
-        chatbot.style.display = "none";
-    }
-}
