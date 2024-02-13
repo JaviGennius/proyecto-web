16fcd83684cc83833ab0a160869b8bd3643dcce7
JaviGennius <javierom93@gmail.com>
2024-02-13 09:22:46 +0100

    flips terminado

--- js/flipimg.js
+++ js/flipimg.js
@@ -1,3 +1,4 @@
+//cardiologos
 let  fotomedico = "delante";
 function flip() {
     let imagen = document.getElementById("cardiologo1");
@@ -5,15 +6,153 @@ function flip() {
         imagen.src = "../imagenes/c2.d.png";
         fotomedico ="detras"
     }
-    else {
-        imagen.src ="../imagenes/cardiolog1.jpg";
-        fotomedico ="delante"
-    }
+  
 
 }
 function flipout(){
-    let imagen = document.getElementById("medico");
+    let imagen = document.getElementById("cardiologo1");
     
         imagen.src ="../imagenes/cardiolog1.jpg";
         fotomedico ="delante"
+}
+
+
+let fotomedico2 = "delante";
+function flip2() {
+    let  imagen = document.getElementById("cardiologo2");
+    if (fotomedico2 =="delante"){
+        imagen.src = "../imagenes/c3.d.png";
+        fotomedico2 ="detras"
     }
+}
+function flipout2(){
+    let imagen = document.getElementById("cardiologo2");
+    imagen.src ="../imagenes/c3.jpg";
+    fotomedico2 ="delante"   
+}
+
+let fotomedico3 ="delante";
+function flip3() {
+    let imagen = document.getElementById("cardiologo3");
+    imagen.src = "../imagenes/c4.d.png";
+    fotomedico3 = "delante"
+}
+function flipout3() {
+    let imagen =document.getElementById("cardiologo3");
+    imagen.src = "../imagenes/c6.jpg";
+    fotomedico3 = "delante"
+}
+//oncologos
+
+let fotomedico4 = "delante";
+function flip4() {
+    let imagen = document.getElementById("oncologo1");
+    imagen.src = "../imagenes/oncologo.d.png";
+    fotomedico4 = "detras"
+}
+function flipout4() {
+    let imagen = document.getElementById("oncologo1");
+    imagen.src = "../imagenes/c21.jpg";
+    fotomedico4 = "delante"
+}
+
+function flip5() {
+        let imagen = document.getElementById("oncologo2");
+        imagen.src = "../imagenes/oncologo2.d.png";
+        fotomedico4 = "detras"
+
+}
+function flipout5() {
+    let imagen = document.getElementById("oncologo2");
+    imagen.src = "../imagenes/c18.jpg";
+    fotomedico4 = "delante"
+}
+function flip6() {
+    let imagen = document.getElementById("oncologo3");
+    imagen.src = "../imagenes/oncologo3.d.png";
+    fotomedico4 = "detras"
+
+}
+function flipout6() {
+let imagen = document.getElementById("oncologo3");
+imagen.src = "../imagenes/c23.jpg";
+fotomedico4 = "delante"
+}
+//traumatologos
+let  fotomedico7 = "delante";
+function flip7() {
+    let imagen = document.getElementById("traumatologo1");
+    if (fotomedico =="delante"){
+        imagen.src = "../imagenes/traumatologo1.d.png";
+        fotomedico ="detras"
+    }
+}
+function flipout7(){
+    let imagen = document.getElementById("traumatologo1");
+    
+        imagen.src ="../imagenes/c14.jpeg";
+        fotomedico ="delante"
+}
+
+
+let fotomedico8 = "delante";
+function flip8() {
+    let  imagen = document.getElementById("traumatologo2");
+    if (fotomedico2 =="delante"){
+        imagen.src = "../imagenes/traumatologo2.d.png";
+        fotomedico2 ="detras"
+    }
+}
+function flipout8(){
+    let imagen = document.getElementById("traumatologo2");
+    imagen.src ="../imagenes/c15.jpeg";
+    fotomedico2 ="delante"   
+}
+
+let fotomedico9 ="delante";
+function flip9() {
+    let imagen = document.getElementById("traumatologo3");
+    imagen.src = "../imagenes/traumatologo3.d.png";
+    fotomedico3 = "delante"
+}
+function flipout9() {
+    let imagen =document.getElementById("traumatologo3");
+    imagen.src = "../imagenes/c12.jpg";
+    fotomedico3 = "delante"
+}
+//neurologos
+
+let fotomedico10 = "delante";
+function flip10() {
+    let imagen = document.getElementById("neurologo1");
+    imagen.src = "../imagenes/neurologo1.d.png";
+    fotomedico4 = "detras"
+}
+function flipout10() {
+    let imagen = document.getElementById("neurologo1");
+    imagen.src = "../imagenes/doctor_1.jpg";
+    fotomedico4 = "delante"
+}
+
+    function flip11() {
+        let imagen = document.getElementById("neurologo2");
+        imagen.src = "../imagenes/neurologo2.d.png";
+        fotomedico4 = "detras"
+
+}
+function flipout11() {
+    let imagen = document.getElementById("neurologo2");
+    imagen.src = "../imagenes/doctor_2.jpeg";
+    fotomedico4 = "delante"
+}
+function flip12() {
+    let imagen = document.getElementById("neurologo3");
+    imagen.src = "../imagenes/neurologo3.d.png";
+    fotomedico4 = "detras"
+
+}
+function flipout12() {
+let imagen = document.getElementById("neurologo3");
+imagen.src = "../imagenes/c10.jpg";
+fotomedico4 = "delante"
+}
