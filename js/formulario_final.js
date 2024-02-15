2cfcdc81a75408b3362c5ec89274d79cfd3e3608
Perales33 <perales.antonio33@hotmail.com>
2024-02-15 16:25:46 +0100

    Cambios antes de la liada hecha por Antonio

--- js/formulario_final.js
+++ js/formulario_final.js
@@ -2,50 +2,47 @@ function validarNombre() {
     var nombre = document.getElementById("nombre");
     var nombreValor = nombre.value.trim();
     var errorNombre = document.getElementById("error_nombre");
-    errorNombre.setAttribute("hidden", true);
-    
     if (nombreValor === "") {
-        errorNombre.removeAttribute("hidden");
+        errorNombre.style.display = "block"
         nombre.style.borderColor = "red";
-        document.getElementById('mensajenombre').style.display = 'block';
         return false;
     } else {
+        errorNombre.style.display = "none";
         nombre.style.borderColor = "green";
-        document.getElementById('mensajenombre').style.display = 'none';
         return true;
     }
 }
+
+
 
 
 function validarPrimerApellido() {
     var primero=document.getElementById("primero");
     var primeroValor = primero.value.trim();
     var errorApellido1=document.getElementById("error_apellido1");
-    errorApellido1.setAttribute("hidden",true);
 
     if (primeroValor === "") {
-        errorApellido1.removeAttribute("hidden")
+        errorApellido1.style.display = "block";
         primero.style.borderColor = "red";
     } else {
-    
+        errorApellido1.style.display = "none";
         primero.style.borderColor = "green";
     }
 }
-    
+
+
 function validarSegundoApellido() {
     var segundo = document.getElementById("segundo");
     var segundoValor = segundo.value.trim();
     var errorApellido2 = document.getElementById("error_apellido2");
-    errorApellido2.setAttribute("hidden",true);
     if (segundoValor === "") {
-        errorApellido2.removeAttribute("hidden");
+        errorApellido2.style.display = "block";
         segundo.style.borderColor = "red";
     } else {
+        errorApellido2.style.display = "none";
         segundo.style.borderColor = "green";
     }
 }
-
-
 
 function validarFechaNacimiento() {
     var dia = document.getElementById("dia").value;
@@ -55,105 +52,92 @@ function validarFechaNacimiento() {
     var mesElement = document.getElementById("mes");
     var anoElement = document.getElementById("ano");
     var errorEdad = document.getElementById("error_edad");
-    errorEdad.setAttribute("hidden",true);
 
     if (dia === "" || mes === "" || ano === "") {
-        errorEdad.removeAttribute("hidden");
+        errorEdad.style.display = "block";
         diaElement.style.borderColor = "red";
         mesElement.style.borderColor = "red";
         anoElement.style.borderColor = "red";
-        document.getElementById('mensajedia').style.display = 'block';
-        document.getElementById('mensajemes').style.display = 'block';
-        document.getElementById('mensajeano').style.display = 'block';
         return false;
     } else {
+        errorEdad.style.display = "none";
         diaElement.style.borderColor = "green";
         mesElement.style.borderColor = "green";
         anoElement.style.borderColor = "green";
-        document.getElementById('mensajedia').style.display = 'none';
-        document.getElementById('mensajemes').style.display = 'none';
-        document.getElementById('mensajeano').style.display = 'none';
         return true;
     }
 }
-
-
-
 
 function validarSexo() {
     var sexo = document.getElementById("sexo");
     var errorSexo = document.getElementById("error_sexo");
-    errorSexo.setAttribute("hidden",true);
     if (sexo.value === "") {
-        errorSexo.removeAttribute("hidden");
+        errorSexo.style.display = "block";
         sexo.style.borderColor = "red";
-        document.getElementById('mensajesexo').style.display = 'block';
         return false;
     } else {
+        errorSexo.style.display = "none";
         sexo.style.borderColor = "green";
-        document.getElementById('mensajesexo').style.display = 'none';
         return true;
     }
 }
+
 
 function validarTelefono() {
     var telefonoInput = document.getElementById("telefono");
     var telefono = telefonoInput.value;
     var telefonoRegex = /^\d+$/;
     var errorTelefono = document.getElementById("error_tel");
-    errorTelefono.setAttribute("hidden",true);
     if (telefono === "" || !telefonoRegex.test(telefono)) {
-        errorTelefono.removeAttribute("hidden");
+        errorTelefono.style.display = "block";
         telefonoInput.style.borderColor = "red";
-        document.getElementById('mensajetelefono').style.display = 'block';
         return false;
     } else {
+        errorTelefono.style.display = "none";
         telefonoInput.style.borderColor = "green";
-        document.getElementById('mensajetelefono').style.display = 'none';
         return true;
     }
 }
-
-
 
 function validarEmail() {
     var emailInput = document.getElementById("email");
     var email = emailInput.value;
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     var errorEmail = document.getElementById("error_email");
-    errorEmail.setAttribute("hidden",true);
     if (email === "" || !emailRegex.test(email)) {
-        errorEmail.removeAttribute("hidden");
+        errorEmail.style.display = "block";
         emailInput.style.borderColor = "red";
-        document.getElementById('mensajecorreo').style.display = 'block';
         return false;
     } else {
+        errorEmail.style.display = "none";
         emailInput.style.borderColor = "green";
-        document.getElementById('mensajecorreo').style.display = 'none';
         return true;
     }
 }
 
+
 function validarFormulario() {
     var esNombreValido = validarNombre();
-    var sonApellidosValidos = validarApellidos();
+    var esPrimerApellidosValido = validarPrimerApellido();
+    let esSegundoApellidoValido = validarSegundoApellido();
     var esFechaNacimientoValida = validarFechaNacimiento();
     var esSexoValido = validarSexo();
     var esTelefonoValido = validarTelefono();
     var esEmailValido = validarEmail();
 
-    if (esNombreValido && sonApellidosValidos && esFechaNacimientoValida && esSexoValido && esTelefonoValido && esEmailValido) {
+
+    if (esNombreValido && esPrimerApellidosValido && esSegundoApellidoValido && esFechaNacimientoValida && esSexoValido && esTelefonoValido && esEmailValido) {
         setTimeout(function() {
             window.location.href = '../index.html'; // Funcion sacada de Chatgpt
-          }, 3000);
+        }, 3000);
         document.getElementById("patientForm").style.display = "none";
         document.getElementById("cabecera").style.display = "none";
         document.getElementById("cargar").style.display = "block";
-
     } else {
         alert("Por favor, complete todos los campos correctamente.");
     }
 }
+
 
 document.getElementById("nombre").addEventListener("blur", validarNombre);
 document.getElementById("primero").addEventListener("blur", validarPrimerApellido);
