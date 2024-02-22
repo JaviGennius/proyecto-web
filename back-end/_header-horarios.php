c33fa4fa6ae26a8fda3cde1a2ef7496c62302edf
Perales33 <perales.antonio33@hotmail.com>
2024-02-22 19:34:18 +0100

    Cambios menu

--- back-end/_header-horarios.php
+++ back-end/_header-horarios.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="../css/ubicacion.css"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384">
     <script src="https://kit.fontawesome.com/e3d46192fc.js" crossorigin="anonymous"></script>
-
 </head>
+<body>
 <header>
         <img src="../imagenes/hospital.png" class="imagenhospital" draggable="false">
         <h1>Horarios y Ubicación del Hospital Felipe VI</h1>
