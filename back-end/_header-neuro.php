b0e93d02c4c151139a55c4f79fe9f123a22e433c
lordly224 <ajuriaolavenachete64@gmail.com>
2024-02-22 19:39:38 +0100

    php - neuro

---
+++ back-end/_header-neuro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Departamento de Neurología</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet"href="../css/departamentos.css">
+    <link rel="icon" type="image/png" href="../imagenes/madrid.png"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384">
+    <script src="https://kit.fontawesome.com/e3d46192fc.js" crossorigin="anonymous"></script>
+    <script src="../js/flipimg.js"></script>
+</head>
+<body>
+    <header id="cabecera">
+        <img src="../imagenes/hospital.png" class="image2"/>
+        <h1>Departamento de Neurología</h1>
+    </header>
+    <?php require_once("_menu.php")?>
