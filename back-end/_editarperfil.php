bd3941852958934cd29abb7122b3a07024d56ba3
Perales33 <perales.antonio33@hotmail.com>
2024-03-02 13:31:51 +0100

    Cambios en editar perfil sin errores

--- back-end/_editarperfil.php
+++ back-end/_editarperfil.php
@@ -1,30 +1,17 @@
-<div class="carrusel">
 <?php
+$estilos = "<link href='../css/edita.css' rel='stylesheet'/>";
+require("_header.php");
 session_start();
 require("initdb.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['edit'])) {
-    // Update personal information
-    $actualizar = "UPDATE Pacientes SET Fecha_nacimiento=?, DNI_paciente=?, Telefono_paciente=?, Correo_paciente=? WHERE DNI_paciente=?";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $actualizar = "UPDATE Pacientes SET Fecha_nacimiento=?, Telefono_paciente=?, Correo_paciente=? WHERE DNI_paciente=?";
     $updateStmt = $con->prepare($actualizar);
-    $updateStmt->bind_param("sssss", $_POST['fecha'], $_POST['DNI'], $_POST['tel'], $_POST['correo'], $_SESSION['dni_usuario']);
+    $updateStmt->bind_param("ssss", $_POST['fecha'], $_POST['tel'], $_POST['correo'], $_SESSION['dni_usuario']);
     $updateStmt->execute();
     $updateStmt->close();
 
-    // Handle photo upload
-    if ($_FILES['foto']['error'] == 0) {
-        $uploadDir = '/imagenes/';
-        $uploadFile = $uploadDir . basename($_FILES['foto']['name']);
-        move_uploaded_file($_FILES['foto']['tmp_name'], $uploadFile);
-
-        // Update the database with the new photo path
-        $updatePhotoQuery = "UPDATE Pacientes SET Foto_usuario=? WHERE DNI_paciente=?";
-        $updatePhotoStmt = $con->prepare($updatePhotoQuery);
-        $newPhotoPath = $uploadFile;
-        $updatePhotoStmt->bind_param("ss", $newPhotoPath, $_SESSION['dni_usuario']);
-        $updatePhotoStmt->execute();
-        $updatePhotoStmt->close();
-    } header('Location: portal_paciente.php'); 
+    header("Location: portal_paciente.php");
     exit();
 }
 
@@ -34,26 +21,24 @@ $stmt->bind_param("s", $_SESSION['dni_usuario']);
 $stmt->execute();
 $result = $stmt->get_result();
 
-while ($row = $result->fetch_assoc()) { 
+while ($row = $result->fetch_assoc()) {
     echo "<div class='informacion'>";
     echo "<form method='post' enctype='multipart/form-data'>";
     echo "<div>";
     echo "<img id='foto' src='" . $row['Foto_usuario'] . "' class='perfil' draggable='false'><br>";
-    echo "<input name='foto' type='file'><br>";
     echo "</div>";
     echo "<br>";
-    echo "<button name='logout' class='boton_cerrar'>Cerrar Sesión</button>";
-    echo "<button name='edit' class='boton_cerrar'>Editar Perfil</button><br>";
-    echo "<h4>{$row['Nombre_paciente']} {$row['Primer_apellido_paciente']} {$row['Segundo_apellido_paciente']}</h4>";
+    echo "<h4>" . $row['Nombre_paciente'] ." " . $row['Primer_apellido_paciente'] . " " . $row['Segundo_apellido_paciente'] . "</h4>";
     echo "<h5> Información Personal</h5>";
-    echo "<label for='fecha'>Fecha de Nacimiento:</label>";
-    echo "<input name='fecha' type='date' value='{$row['Fecha_nacimiento']}'><br>";
+    echo "<p><label for='fecha'>Fecha de Nacimiento:</label>";
+    echo "<input name='fecha' type='date' value='" . $row['Fecha_nacimiento'] ."'</p><br>";
     echo "<h5>Información de Contacto:</h5>";
-    echo "<label for='tel'>Teléfono:</label>";
-    echo "<input name='tel' type='text' value='{$row['Telefono_paciente']}'><br>";
-    echo "<label for='correo'>Correo:</label>";
-    echo "<input name='correo' type='email' value='{$row['Correo_paciente']}'><br>";
+    echo "<p><label for='tel'>Teléfono:</label>";
+    echo "<input name='tel' type='text' value='" . $row['Telefono_paciente'] . "'</p><br>";
+    echo "<p><label for='correo'>Correo:</label>";
+    echo "<input name='correo' type='email' value='" . $row['Correo_paciente'] ."'</p><br><br>";
     echo "<input type='submit' value='Guardar Cambios'>";
+    echo "<a href='portal_paciente.php'><input type='button' name='volver' value='Volver Sin Cambios'></a>";
     echo "</form>";
     echo "</div>";
 }
@@ -62,5 +47,4 @@ $stmt->close();
 $con->close();
 ?>
 
-<hr>
-<div class="ingresos">
+
