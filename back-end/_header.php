069182ca02d18e4156c9e79580e5b71ba7e224cb
Perales33 <perales.antonio33@hotmail.com>
2024-03-07 19:01:11 +0100

    Comentarios

--- back-end/_header.php
+++ back-end/_header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //Variables dependiendiendo del archivo (Ayuda de Adrián)
+    <!--Variables dependiendiendo del archivo (Ayuda de Adrián)-->
     <title><?= $titulo?></title>
     <?= $script ?>
     <?= $estilos ?>
