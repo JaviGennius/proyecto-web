f21bc26059201e3bde618f933970097bd7c94264
lordly224 <ajuriaolavenachete64@gmail.com>
2024-03-07 13:05:01 +0100

    chatbot - php

--- js/chatbotcopia.js
+++ js/chatbotcopia.js
@@ -10,11 +10,16 @@ function openchatbot() {
       chatbot.style.display = "none";
   }
 }
+// Espera a que el documento esté listo para ejecutar el código
 $(document).ready(function() {
-  $("#send-btn").on("click", function() {
-      $value = $("#data").val();
+  $("#send-btn").on("click", function() {     // Asociamos el evento clic al botón de enviar
+      $value = $("#data").val();                // Obtenemos el valor del campo de entrada
+
+    // Creamos el mensaje del usuario y lo agregamos al contenedor del chat
       $msg = '<div class="user-inbox inbox user"><div class="msg-header"><p>Usuario: ' + $value + '</p></div></div>';
       $(".form").append($msg);
+
+    // Limpiamos el campo de entrada
       $("#data").val('');
 
 
